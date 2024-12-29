5598bc9d3b90e5571a5bedba345d2393d851d836
Mariana <108686064+Mariana-rc01@users.noreply.github.com>
2024-12-29 20:54:37 +0000

    feat: 4.1 and 4.2 (#3)

--- cp2425t/Expr.hs
+++ cp2425t/Expr.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE NPlusKPatterns #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-noncanonical-monad-instances #-}
 
 module Expr where
 import Cp
@@ -14,29 +16,51 @@ soma  x y = T Add [x,y]
 multi x y = T Mul [x,y]
 ite x y z = T ITE [x,y,z]
 
+inExpr :: Either a (Either b (Op, [Expr b a])) -> Expr b a
 inExpr = either V (either N (uncurry T))
-outExpr = undefined
+
+outExpr :: Expr b a -> Either a (Either b (Op, [Expr b a]))
+outExpr (V n) = i1 n
+outExpr (N n) = (i2.i1) n
+outExpr (T op exprs) = (i2.i2) (op,exprs)
+
 
 baseExpr g h f = g -|- (h -|- id >< map f)
 
 -- (2) Ana + cata + hylo -------------------------------------------------------
 
-recExpr = undefined
+recExpr = baseExpr id id
 
-cataExpr g = undefined
+cataExpr g = g . recExpr (cataExpr g) . outExpr
 
-anaExpr g = undefined
-                
-hyloExpr h g = undefined
+anaExpr g = inExpr . recExpr (anaExpr g) . g
+
+hyloExpr h g = cataExpr h . anaExpr g
 
 -- (3) Monad -------------------------------------------------------------------
 
-{-instance Monad LTree where
-     return  = Leaf
-     t >>= g = (mu . fmap g) t
+instance Functor (Expr b)
+     where fmap f = cataExpr (inExpr . baseExpr f id id)
 
-mu  :: LTree (LTree a) -> LTree a
-mu  =  cataLTree (either id Fork)-}
+instance Applicative (Expr b) where
+    pure :: a -> Expr b a
+    pure = V
+    (V f) <*> x = fmap f x
+    (N b) <*> _ = N b
+    (T op fs) <*> x = T op (map (<*> x) fs)
+
+instance Monad (Expr b) where
+    return :: a -> Expr b a
+    return = pure
+
+    (>>=) :: Expr b a -> (a -> Expr b b1) -> Expr b b1
+    t >>= g = mu (fmap g t)
+
+mu :: Expr b (Expr b a) -> Expr b a
+mu  =  cataExpr (either id (inExpr . i2))
+
+u :: a -> Expr b a
+u = V
 
 -- Exemplos
 e = ite (V "x") (N 0) (multi (V "y") (soma (N 3) (V "y")))
