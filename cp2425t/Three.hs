b13e490eb46594317bd5042c2bf9ba37292eeee0
Rita Camacho <ritacunhacamacho@gmail.com>
2025-01-12 19:39:35 +0000

    feat: add 3 explanation to report

--- cp2425t/Three.hs
+++ cp2425t/Three.hs
@@ -9,29 +9,29 @@ import Cp
 import Data.List (tails)
 
 -- Resolução sem CP
-convolve1 :: (Num a) => [a] -> [a] -> [a]
-convolve1 hs xs =
-    let pad = replicate (length hs - 1) 0
-        ts  = pad ++ xs
-    in map (sum . zipWith (*) (reverse hs)) (init $ tails ts)
-
--- Resolução com CP 1
-convolve2 :: Num a => [a] -> [a] -> [a]
-convolve2 l1 l2 = anaList (anaGene2 l1) (addzeros l2)
-    where
-      addzeros l = replicate (length l1 - 1) 0 ++ l2
-
-anaGene2 :: Num a => [a] -> [a] -> Either () (a, [a])
-anaGene2 l1 l2
-    | null l2 = i1 ()
-    | otherwise = i2 (sum (zipWith (*) (reverse l1) (take (length l1) l2)), tail l2)
+-- convolve1 :: (Num a) => [a] -> [a] -> [a]
+-- convolve1 hs xs =
+    -- let pad = replicate (length hs - 1) 0
+        -- ts  = pad ++ xs
+    -- in map (sum . zipWith (*) (reverse hs)) (init $ tails ts)
 
 -- Resolução com CP 2
-convolve3 :: Num a => [a] -> [a] -> [a]
-convolve3 l1 l2 = anaList (anaGene3 l1 l2) 0
+-- convolve2 :: Num a => [a] -> [a] -> [a]
+-- convolve2 l1 l2 = anaList (anaGene2 l1) (addzeros l2)
+    -- where
+      -- addzeros l = replicate (length l1 - 1) 0 ++ l2
 
-anaGene3 :: Num a => [a] -> [a] -> Int -> Either () (a, Int)
-anaGene3 l1 l2 = cond (>= m + n - 1) (const $ i1 ())
+-- anaGene2 :: Num a => [a] -> [a] -> Either () (a, [a])
+-- anaGene2 l1 l2
+    --  | null l2 = i1 ()
+    --  | otherwise = i2 (sum (zipWith (*) (reverse l1) (take (length l1) l2)), tail l2)
+
+-- Resolução com CP (Final)
+convolve :: Num a => [a] -> [a] -> [a]
+convolve l1 l2 = anaList (anaGene l1 l2) 0
+
+anaGene :: Num a => [a] -> [a] -> Int -> Either () (a, Int)
+anaGene l1 l2 = cond (>= m + n - 1) (const $ i1 ())
                     (\i -> i2 (sum  $ zipWith (*) l1 (map (\j -> access (l2, (i, j))) [0..(m - 1)]), i + 1))
     where
         m = length l1; n = length l2
